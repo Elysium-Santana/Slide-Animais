31a52eeee3afc74e55a8f7a739cf2c4a438a44b2
Elysium Santata <camaleplata@outlook.com>
2022-08-01 03:16:26 -0300

    adicionando calculo de distancias e controle de mouse

--- js/slide.js
+++ js/slide.js
@@ -2,19 +2,29 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, moviment: 0 };
+  }
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0 )`;
+  }
+  updatePosition(clientX) {
+    this.dist.moviment = this.dist.startX - clientX;
+    return this.dist.finalPosition - this.dist.moviment;
   }
 
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
-    console.log("clicou");
   }
   onMove(event) {
-    console.log("moveu");
+    const finalPosition = this.updatePosition(event.clientX) * 1.5;
+    this.moveSlide(finalPosition);
   }
   onEnd(event) {
     this.wrapper.removeEventListener("mousemove", this.onMove);
-    console.log("acabou");
+    this.dist.finalPosition = this.dist.movePosition;
   }
   bindEvent() {
     this.onStart = this.onStart.bind(this);
